d12e4a5981af299a747a11694b011b6dda90fe91
LE2HE <leehe4352@gamil.com>
2022-07-09 01:00:31 +0900

    Implement #3 : Service 저장 기능 구현
    save 기능
    - repository에 저장하는 기능

---
+++ src/main/java/com/example/springboot/springbootwebservice/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.example.springboot.springbootwebservice.service.posts;
+
+import com.example.springboot.springbootwebservice.domain.posts.PostsRepository;
+import com.example.springboot.springbootwebservice.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
