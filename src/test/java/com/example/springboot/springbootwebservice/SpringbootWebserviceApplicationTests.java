15fbdedeeaf3817abc34cb5470bc948989489c61
LE2HE <leehe4352@gamil.com>
2022-07-08 09:50:39 +0900

    Setup SpringBoot project
    Java + Gradle Project Setup

---
+++ src/test/java/com/example/springboot/springbootwebservice/SpringbootWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.springbootwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
